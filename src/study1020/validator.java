ffbfa8fc81d3d4ffc008a20622e5e7a8a65338c8
song <sgh83037@naver.com>
2021-10-20 22:12:27 +0900

    1020

---
+++ src/study1020/validator.java
@@ -0,0 +1,22 @@
+package study1020;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+
+public class validator {
+/*	@PostMapping("join")
+	public String join(@Validated JoinForm form
+						,Errors errors  //Errors 객체는 반드시 검증할 객체의 바로 뒤에 작성
+			) {
+		
+		if(errors.hasErrors()) {
+			return "member/join-form";
+		}
+		
+		logger.debug("member : {}",form.toString());
+		memberService.insertMember(form);
+		return "index";
+	}
+
+*/
+}
