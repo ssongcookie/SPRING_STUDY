46f98fe34ad07138fb52f2405ee9dae4c195fe40
song <sgh83037@naver.com>
2022-01-19 23:30:56 +0900

    Input/Output

---
+++ src/studyJava/java/Study10.java
@@ -0,0 +1,24 @@
+package studyJava.java;
+
+
+public class Study10 {
+
+	public static void main(String[] args) {
+	//FileInputStream
+	//	read(); 1byte씩 읽고
+	//	read(byte[]);  []크기만큼 읽고
+		 
+	//FileOutputStream
+	//	write(); 
+	//	write(byte[] b); 전체쓰기
+	
+	
+	//input output 은 반드시 예외처리 해야함
+	
+	//DataInputStream, DataOutputStream
+	//byte 단위의 입출력을 개선해서 문자열을 좀 더 편리하게 다룰 수 있다.
+		
+	//BufferedReader, BufferedWriter
+	//byte 단위의 입출력을 개선해서 문자열을 좀 더 편리하게 다룰 수 있다.
+	}
+}
